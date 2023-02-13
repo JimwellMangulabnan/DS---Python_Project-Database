91cf2421a4a8aa1bd27a633ccb853a179d5e2c33
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-02-13 15:05:26 +0800

    Adding Widgets to Left Body Frame together with their label

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ class Product:
         self.root.geometry("1325x690")
         self.root.config(bg="yellow")
 
+        pId = StringVar()
+        pName = StringVar()
+        pPrice = StringVar()
+        pQty = StringVar()
+        pCompany = StringVar()
+        pContact = StringVar()
+
         ''' Create the frame'''
         MainFrame = Frame(self.root,bg="red")
         MainFrame.grid()
@@ -40,8 +47,38 @@ class Product:
 
         RightBodyFrame = LabelFrame(BodyFrame, bd=2, width=300, height=380,
                                    padx=20, pady=10, bg='yellow', relief=RIDGE, font=('arial', 15, 'bold'),
-                                   text='Product Item Details: ')
+                                   text='Product Item Information: ')
         RightBodyFrame.pack(side=RIGHT)
+
+        ''' Add the Widgets to LeftBodyFrame'''
+
+        self.labelpId=Label(LeftBodyFrame, font=('arial', 15, 'bold'),
+                             text="Product Id : ", padx =2, bg='white', fg = 'blue')
+        self.labelpId.grid(row=0, column=0, sticky= W)
+
+        self.txtpId = Entry(LeftBodyFrame, font=('arial', 20, 'bold'),
+                            textvariable=pId, width=35)
+        self.txtpId.grid(row=0, column=1, sticky= W)
+
+        self.labelpName = Label(LeftBodyFrame, font=('arial', 15, 'bold'),
+                              text="Product Name : ", padx=2, bg='white', fg='blue')
+        self.labelpName.grid(row=1, column=0, sticky=W)
+
+        self.labelpPrice = Label(LeftBodyFrame, font=('arial', 15, 'bold'),
+                              text="Product Price : ", padx=2, bg='white', fg='blue')
+        self.labelpPrice.grid(row=2, column=0, sticky=W)
+
+        self.labelpQty = Label(LeftBodyFrame, font=('arial', 15, 'bold'),
+                              text="Product Quantity : ", padx=2, bg='white', fg='blue')
+        self.labelpQty.grid(row=3, column=0, sticky=W)
+
+        self.labelpCompany = Label(LeftBodyFrame, font=('arial', 15, 'bold'),
+                              text="Mfg. Company : ", padx=2, bg='white', fg='blue')
+        self.labelpCompany.grid(row=4, column=0, sticky=W)
+
+        self.labelpContact = Label(LeftBodyFrame, font=('arial', 15, 'bold'),
+                              text="Company Contact", padx=2, bg='white', fg='blue')
+        self.labelpContact.grid(row=5, column=0, sticky=W)
 
 
 

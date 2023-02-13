98a94f372ccde8d6b836534f384e03b60afd7ef4
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-02-13 14:40:17 +0800

    another Frame for bodyframe and its leftbody frame has been made

--- main.py
+++ main.py
@@ -22,14 +22,23 @@ class Product:
         HeadFrame.pack(side=TOP)
 
         self.ITitle = Label(HeadFrame, font=('arial', 50, 'bold'), fg='red',
-                            text='Warehouse Inventory Sales Purchase', bg='white')
+                            text='Warehouse Inventory Sales Purchase ', bg='white')
         self.ITitle.grid()
 
-        OperationFrame = Frame(MainFrame, bd=1, width=1300, height=60,
+        OperationFrame = Frame(MainFrame, bd=2, width=1300, height=60,
                                padx=50, pady=20,bg='white', relief=RIDGE)
         OperationFrame.pack(side=BOTTOM)
 
-        
+        BodyFrame = Frame(MainFrame, bd=2, width=1290, height=400,
+                               padx=30, pady=20, bg='white', relief=RIDGE)
+        BodyFrame.pack(side=BOTTOM)
+
+        LeftBodyFrame = LabelFrame(BodyFrame, bd=2, width=600, height=380,
+                          padx=20, pady=10, bg='yellow', relief=RIDGE, font=('arial', 15, 'bold'),
+                              text= 'Product Item Details: ')
+        LeftBodyFrame.pack(side=LEFT)
+
+
 
 
 if __name__ =='__main__':

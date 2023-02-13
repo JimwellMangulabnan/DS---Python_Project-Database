ede366cc011fe6dd4f749130bc22d0bc27f79bb2
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-02-13 14:22:23 +0800

    using a class for Front End UI (user interface)

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ from tkinter import *
 import tkinter.messagebox
 import sqlite3
 
+#class for front End UI (user interface)
+class Product:
 
+    def __init__(self,root):
+
+        self.root = root
+        self.root.title("WAREHOUSE INVENTORY SALES PURCHASE MANAGEMENT SYSTEM")
+        self.root.geometry("1325x690")
+        self.root.config(bg="yellow")
+
+        
+
+
+if __name__ =='__main__':
+    root=Tk()
+    application = Product(root)
+    root.mainloop()
